a62a5426cc8983358e352b2f49e9917f0dca17f2
suraj <surajsapkal264@gmail.com>
2020-07-23 17:32:10 +0530

    Changed UI

--- app/src/main/java/com/example/learnkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learnkotlin/MainActivity.kt
@@ -5,90 +5,162 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ExpandableListView
+import android.widget.TextView
 import android.widget.Toast
+import com.diegodobelo.expandingview.ExpandingItem
+import com.diegodobelo.expandingview.ExpandingList
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.subtopic.*
+import kotlinx.android.synthetic.main.subtopic.view.*
+import kotlinx.android.synthetic.main.topic.view.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var listViewAdapter: ExpandableListViewAdapter
-    private lateinit var topicList: List<String>
-    private lateinit var subTopicList: HashMap<String,List<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        showList()
+        val mainList = findViewById<ExpandingList>(R.id.mainList)
+        topicOne()
+        topicTwo()
+        topicThree()
+        topicFour()
 
-        listViewAdapter = ExpandableListViewAdapter(this,topicList,subTopicList)
-        eListView.setAdapter(listViewAdapter)
-
-        eListView.setOnChildClickListener { parent:ExpandableListView, v: View, groupPosition:Int , childPosition:Int, id:Long ->
-            val selected: String = listViewAdapter.getChild(groupPosition, childPosition).toString()
-            when(selected){
+        subTopicTitle.setOnClickListener {
+            when(subTopicTitle.text){
                 "Kotlin Hello World"->{
-                    val browserIntent = Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://www.youtube.com/watch?v=1676actiiME&list=PLrnPJCHvNZuAIbejjZA1kGfLeA8ZpICB2&index=5&t=0s"))
-                    startActivity(browserIntent)
+                    val intent = Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.youtube.com/watch?v=1676actiiME&list=PLrnPJCHvNZuAIbejjZA1kGfLeA8ZpICB2&index=5&t=5s"))
+                    startActivity(intent)
                 }
             }
-            return@setOnChildClickListener true
         }
+
 
     }
 
-    private fun showList() {
-        topicList = ArrayList()
-        subTopicList = HashMap()
+    private fun topicOne(){
+        val topic1:ExpandingItem = mainList.createNewItem(R.layout.expanding_topic_1)
+        topic1.findViewById<TextView>(R.id.topicTitle).text = "Kotlin Introduction"
+        topic1.setIndicatorColorRes(R.color.colorOrange)
+        topic1.setIndicatorIconRes(R.drawable.ic_add)
+        topic1.createSubItems(7)
 
-        (topicList as ArrayList<String>).add("Kotlin Introduction")
-        (topicList as ArrayList<String>).add("Kotlin Flow Control")
-        (topicList as ArrayList<String>).add("Kotlin Functions")
-        (topicList as ArrayList<String>).add("Kotlin OOP")
+        val t1subtopic0: View = topic1.getSubItemView(0)
+        t1subtopic0.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin Hello World"
 
-        val subtopic1: MutableList<String> = ArrayList()
-        subtopic1.add("Kotlin Hello World")
-        subtopic1.add("Kotlin Data Types")
-        subtopic1.add("Kotlin Operators")
-        subtopic1.add("Kotlin Type Conversion")
-        subtopic1.add("Kotlin Expression and Statement")
-        subtopic1.add("Kotlin Comments")
-        subtopic1.add("Kotlin Input/Output")
+        val t1subtopic1: View = topic1.getSubItemView(1)
+        t1subtopic1.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin Data Types"
 
-        val subtopic2: MutableList<String> = ArrayList()
-        subtopic2.add("Kotlin if expression")
-        subtopic2.add("Kotlin when Expression")
-        subtopic2.add("Kotlin while Loop")
-        subtopic2.add("Kotlin for Loop")
-        subtopic2.add("Kotlin break")
-        subtopic2.add("Kotlin continue")
+        val t1subtopic2: View = topic1.getSubItemView(2)
+        t1subtopic2.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin Operators"
 
-        val subtopic3: MutableList<String> = ArrayList()
-        subtopic3.add("Kotlin function")
-        subtopic3.add("Infix Function Call")
-        subtopic3.add("default and Named Arguments")
-        subtopic3.add("Recursion and Tail Recursion")
+        val t1subtopic3: View = topic1.getSubItemView(3)
+        t1subtopic3.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin Type Conversion"
 
-        val subtopic4: MutableList<String> = ArrayList()
-        subtopic4.add("Kotlin Class and Objects")
-        subtopic4.add("Kotlin Constructors")
-        subtopic4.add("Kotlin Getters and Setters")
-        subtopic4.add("Kotlin Inheritance")
-        subtopic4.add("Kotlin Visibility Modifiers")
-        subtopic4.add("Kotlin Abstract Class")
-        subtopic4.add("Kotlin Interface")
-        subtopic4.add("Kotlin Nested and Inner Classes")
-        subtopic4.add("Kotlin Data Class")
-        subtopic4.add("Kotlin Sealed Class")
-        subtopic4.add("Kotlin Object")
-        subtopic4.add("Kotlin Companion Objects")
-        subtopic4.add("Kotlin Extension Function")
-        subtopic4.add("Kotlin Operator Overloading")
+        val t1subtopic4: View = topic1.getSubItemView(4)
+        t1subtopic4.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin Expression & Statement"
 
-        subTopicList[topicList[0]] = subtopic1
-        subTopicList[topicList[1]] = subtopic2
-        subTopicList[topicList[2]] = subtopic3
-        subTopicList[topicList[3]] = subtopic4
+        val t1subtopic5: View = topic1.getSubItemView(5)
+        t1subtopic5.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin Comments"
+
+        val t1subtopic6: View = topic1.getSubItemView(6)
+        t1subtopic6.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin Input/Otuput"
+    }
+
+    private fun topicTwo(){
+        val topic2:ExpandingItem = mainList.createNewItem(R.layout.expanding_topic_2)
+        topic2.findViewById<TextView>(R.id.topic2Title).text = "Kotlin Flow Control"
+        topic2.setIndicatorColorRes(R.color.colorRed)
+        topic2.setIndicatorIconRes(R.drawable.ic_add)
+        topic2.createSubItems(6)
+
+        val t2subtopic0: View = topic2.getSubItemView(0)
+        t2subtopic0.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin if expression"
+
+        val t2subtopic1: View = topic2.getSubItemView(1)
+        t2subtopic1.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin when Expression"
+
+        val t2subtopic2: View = topic2.getSubItemView(2)
+        t2subtopic2.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin while Loop"
+
+        val t2subtopic3: View = topic2.getSubItemView(3)
+        t2subtopic3.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin for Loop"
+
+        val t2subtopic4: View = topic2.getSubItemView(4)
+        t2subtopic4.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin break"
+
+        val t2subtopic5: View = topic2.getSubItemView(5)
+        t2subtopic5.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin continue"
+    }
+
+    private fun topicThree(){
+        val topic3:ExpandingItem = mainList.createNewItem(R.layout.expanding_topic_3)
+        topic3.findViewById<TextView>(R.id.topic3Title).text = "Kotlin Functions"
+        topic3.setIndicatorColorRes(R.color.colorYellow)
+        topic3.setIndicatorIconRes(R.drawable.ic_add)
+        topic3.createSubItems(4)
+
+        val t3subtopic0: View = topic3.getSubItemView(0)
+        t3subtopic0.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin function"
+
+        val t3subtopic1: View = topic3.getSubItemView(1)
+        t3subtopic1.findViewById<TextView>(R.id.subTopicTitle).text = "Infix Function Call"
+
+        val t3subtopic2: View = topic3.getSubItemView(2)
+        t3subtopic2.findViewById<TextView>(R.id.subTopicTitle).text = "Default & Named Arguments"
+
+        val t3subtopic3: View = topic3.getSubItemView(3)
+        t3subtopic3.findViewById<TextView>(R.id.subTopicTitle).text = "Recursion & Tail Recursion"
+    }
+
+    private fun topicFour(){
+        val topic4:ExpandingItem = mainList.createNewItem(R.layout.expanding_topic_4)
+        topic4.findViewById<TextView>(R.id.topic4Title).text = "Kotlin OOP"
+        topic4.setIndicatorColorRes(R.color.colorGreen)
+        topic4.setIndicatorIconRes(R.drawable.ic_add)
+        topic4.createSubItems(14)
+
+        val t4subtopic0: View = topic4.getSubItemView(0)
+        t4subtopic0.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin function"
+
+        val t4subtopic1: View = topic4.getSubItemView(1)
+        t4subtopic1.findViewById<TextView>(R.id.subTopicTitle).text = "Infix Function Call"
+
+        val t4subtopic2: View = topic4.getSubItemView(2)
+        t4subtopic2.findViewById<TextView>(R.id.subTopicTitle).text = "Default & Named Arguments"
+
+        val t4subtopic3: View = topic4.getSubItemView(3)
+        t4subtopic3.findViewById<TextView>(R.id.subTopicTitle).text = "Recursion & Tail Recursion"
+
+        val t4subtopic04: View = topic4.getSubItemView(4)
+        t4subtopic04.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin function"
+
+        val t4subtopic5: View = topic4.getSubItemView(5)
+        t4subtopic5.findViewById<TextView>(R.id.subTopicTitle).text = "Infix Function Call"
+
+        val t4subtopic6: View = topic4.getSubItemView(6)
+        t4subtopic6.findViewById<TextView>(R.id.subTopicTitle).text = "Default & Named Arguments"
+
+        val t4subtopic7: View = topic4.getSubItemView(7)
+        t4subtopic7.findViewById<TextView>(R.id.subTopicTitle).text = "Recursion & Tail Recursion"
+
+        val t4subtopic8: View = topic4.getSubItemView(8)
+        t4subtopic8.findViewById<TextView>(R.id.subTopicTitle).text = "Kotlin function"
+
+        val t4subtopic9: View = topic4.getSubItemView(9)
+        t4subtopic9.findViewById<TextView>(R.id.subTopicTitle).text = "Infix Function Call"
+
+        val t4subtopic10: View = topic4.getSubItemView(10)
+        t4subtopic10.findViewById<TextView>(R.id.subTopicTitle).text = "Default & Named Arguments"
+
+        val t4subtopic11: View = topic4.getSubItemView(11)
+        t4subtopic11.findViewById<TextView>(R.id.subTopicTitle).text = "Recursion & Tail Recursion"
+
+        val t4subtopic12: View = topic4.getSubItemView(12)
+        t4subtopic12.findViewById<TextView>(R.id.subTopicTitle).text = "Default & Named Arguments"
+
+        val t4subtopic13: View = topic4.getSubItemView(13)
+        t4subtopic13.findViewById<TextView>(R.id.subTopicTitle).text = "Recursion & Tail Recursion"
     }
 }
